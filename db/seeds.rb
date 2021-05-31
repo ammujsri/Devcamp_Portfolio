02aecfc3493c047033c207d80558c37d5fe40f2e
Gayathri Sritharan <gayathri@Gayathris-MacBook-Pro.local>
2021-05-31 19:33:52 +0530

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,4 +54,12 @@ puts "3 topics created"
  end
 
  puts "9 portfolio items created" 
+
+ 3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
  
